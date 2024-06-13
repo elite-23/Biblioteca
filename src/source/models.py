b4e6836cb6030623699208215375b1621ba73a5a
andbar27 <andrea.barbato2796@icloud.com>
2024-06-13 12:39:37 +0200

    starting models

--- src/source/models.py
+++ src/source/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+
+    book_id: str = models.CharField(max_length=9, primary_key=True)
+
+    title: str = models.CharField(max_length=50)
+
+    author: str = models.CharField(max_length=25)
+
+    is_borrowed: bool = False
+
+    is_expired: bool = False
+
+
+class Member(models.Model):
+    
+    member_id: str = models.CharField(max_length=9, primary_key=True)
+
+    name: str = models.CharField(max_length=25)
+
+    borrowed_books: list[Book] = []
